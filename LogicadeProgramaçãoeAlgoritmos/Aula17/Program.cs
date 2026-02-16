58f7e0245b770975521aafa3e875be59d7cb1135
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-02-16 15:07:09 -0300

    Implementado conteúdo da aula 17 do curso
    Aula contêm exemplos referente ao assunto abordado em aula sobre Processamento de dados em C# e Casting como:
     - <variável> = <expressão>;
     - resultado = (double) a / b;

---
+++ LogicadeProgramaçãoeAlgoritmos/Aula17/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Exemplo 1
+
+            int x, y;
+
+            x = 5;
+
+            y = 2 * x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.ReadLine();
+
+            //Exemplo 2
+
+            int x;
+            double y;
+
+            x = 5;
+
+            y = 2 * x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.ReadLine();
+
+            //Exemplo 3
+
+            double b, B, h, area;
+
+            b = 6.0;
+            B = 8.0;
+            h = 5.0;
+
+            area = (b + B) / 2.0 * h;
+
+            Console.WriteLine(area);
+
+            Console.ReadLine();
+
+            //Exemplo 4
+
+            int a, b;
+            double resultado;
+
+            a = 5;
+            b = 2;
+
+            resultado = a / b;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+
+            //Exemplo 4 Correto
+
+            int a, b;
+            double resultado;
+
+            a = 5;
+            b = 2;
+
+            resultado = (double) a / b;
+
+            Console.WriteLine (resultado);
+
+            *///Exemplo 5
+
+            double a;
+            int b;
+
+            a = 5.0;
+            b = a;
+            
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- LogicadeProgramaçãoeAlgoritmos/Aula17/obj/Debug/net10.0/Aula17.AssemblyInfo.cs
+++ LogicadeProgramaçãoeAlgoritmos/Aula17/obj/Debug/net10.0/Aula17.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Aula16")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Aula17")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4c170ef08a8e09fbbde6faba9e6d3075f67d3d")]
-[assembly: System.Reflection.AssemblyProductAttribute("Aula16")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Aula16")]
+[assembly: System.Reflection.AssemblyProductAttribute("Aula17")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Aula17")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
