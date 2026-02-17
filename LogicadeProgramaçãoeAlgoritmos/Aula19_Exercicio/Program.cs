911132ee8164340f6867f968fbe2b60103c3ec28
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-02-17 18:52:15 -0300

    Implementado exercícios referente a aula 19 do curso

---
+++ LogicadeProgramaçãoeAlgoritmos/Aula19_Exercicio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Entre com seu nome completo: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Quantos quartos tem na sua casa? ");
+            int quartos = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Entre seu último nome, idade e altura (na mesma linha): ");
+            string[] vet = Console.ReadLine().Split(' ');
+            string unome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco);
+            Console.WriteLine(unome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
