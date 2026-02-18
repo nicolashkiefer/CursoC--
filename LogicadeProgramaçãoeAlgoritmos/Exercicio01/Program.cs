1939d1e30a87bd16ee879ad9caf1a48ce316cd6d
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-02-18 18:58:16 -0300

    Implementado primeiro Exercício prático do curso Aula 21
    Primeiro exercício prático do curso onde é abordado parte do conteúdo  apresentado, realizando a criação de variáveis, convertendo para tipo específico e escrevendo o resultado de contas na tela.

---
+++ LogicadeProgramaçãoeAlgoritmos/Exercicio01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("***Calculado de Área e Preço de Terreno***");
+            Console.Write("Largura do Terreno: ");
+            double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Comprimento do Terreno: ");
+            double comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Informe o valor por metro quadrado: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("ÁREA: " + (largura*comprimento).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PREÇO: " + ((largura*comprimento)*valor).ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
