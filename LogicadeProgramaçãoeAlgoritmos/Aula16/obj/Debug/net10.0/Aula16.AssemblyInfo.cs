da8401993ecda80cb402a8f11597e00b8fe6641a
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-02-24 15:49:59 -0300

    Implementado 3º Exercício prático do curso

--- LogicadeProgramaçãoeAlgoritmos/Aula16/obj/Debug/net10.0/Aula16.AssemblyInfo.cs
+++ LogicadeProgramaçãoeAlgoritmos/Aula16/obj/Debug/net10.0/Aula16.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f28a9173bf2bd3e6998a95dcdeb69bcc86bf3fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd7c23aeba422069d0d848621e90ee44913ba498")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula16")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ LogicadeProgramaçãoeAlgoritmos/Exercicio03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro nome e idade de uma pessoa: ");
+            string[] pessoa = Console.ReadLine().Split(' ');
+            string nomeP1 = pessoa[0];
+            int idadeP1 = int.Parse(pessoa[1], CultureInfo.InvariantCulture);
+           
+            Console.Write("Digite outro primeiro nome e idade de uma pessoa: ");
+            pessoa = Console.ReadLine().Split(' ');
+            string nomeP2 = pessoa[0];
+            int idadeP2 = int.Parse(pessoa[1], CultureInfo.InvariantCulture);
+
+            double media = (double)(idadeP1 + idadeP2) / 2.0;
+
+            Console.WriteLine("A idade médio de " + nomeP1 + " e " + nomeP2 +" é de " + media.ToString("F1", CultureInfo.InvariantCulture) + " anos!");
+
+            Console.ReadLine();
+        }
+    }
+}
