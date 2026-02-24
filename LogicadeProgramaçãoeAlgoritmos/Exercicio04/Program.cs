854500922f08e2645a446adcfac429e33601ed98
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-02-24 18:33:54 -0300

    Implementado 4º Exercício prático do curso

---
+++ LogicadeProgramaçãoeAlgoritmos/Exercicio04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio04
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            Console.WriteLine("//////// Conversão de Tempo ////////");
+            Console.Write("Informe um valor: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int horas = N / 3600;
+            int sobra = N % 3600;
+
+            int minutos = sobra / 60;
+            int segundos = sobra % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+
+            Console.ReadLine();
+        }
+    }
+}
