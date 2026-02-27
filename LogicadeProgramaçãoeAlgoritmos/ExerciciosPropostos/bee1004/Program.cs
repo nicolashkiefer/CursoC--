8e1f412c634cd2ef7479f0ade53835b96555eab7
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-02-27 17:38:53 -0300

    Adicionado o exercício bee1004
    Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.
    
    Entrada
    O arquivo de entrada contém 2 valores inteiros.
    
    Saída
    Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.

---
+++ LogicadeProgramaçãoeAlgoritmos/ExerciciosPropostos/bee1004/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace bee1004
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            int PROD = A * B;
+
+            Console.WriteLine("PROD = "+ PROD);
+        }
+    }
+}
