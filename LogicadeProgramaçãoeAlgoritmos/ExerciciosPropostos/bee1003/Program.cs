b70b5554204a0794b31b0f66340f00f06b78af4d
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-02-27 15:21:59 -0300

    Atualizado o exercício bee1003

--- LogicadeProgramaçãoeAlgoritmos/ExerciciosPropostos/bee1003/Program.cs
+++ LogicadeProgramaçãoeAlgoritmos/ExerciciosPropostos/bee1003/Program.cs
@@ -8,7 +8,13 @@ namespace bee1003
     {
         static void Main(String[] args)
         {
-            
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            int SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+
         }
     }
 }
