1745d7eabe1809c0ffcbe809a65ea3c090dad295
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-02-27 18:17:57 -0300

    Adicionado o exercício bee1005
    Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
    
    Entrada
    O arquivo de entrada contém 2 valores com uma casa decimal cada um.
    
    Saída
    Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 5 dígitos após o ponto decimal e com um espaço em branco antes e depois da igualdade. Utilize variáveis de dupla precisão (double) e como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".

---
+++ LogicadeProgramaçãoeAlgoritmos/ExerciciosPropostos/bee1005/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace bee1005
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Média
+            double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double MEDIA = ((A * 3.5) + (B * 7.5)) / 11.0; 
+
+            Console.WriteLine("MEDIA = "+ MEDIA.ToString("F5", CultureInfo.InvariantCulture));
+        }
+    }
+}
