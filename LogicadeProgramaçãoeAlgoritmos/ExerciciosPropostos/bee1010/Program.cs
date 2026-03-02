2588fd227b4eb9da64ff5846f95664b73c215d71
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-03-02 17:50:29 -0300

    Adicionado o exercício bee1010
    Cálculo Simples
    
    Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
    
    Entrada
    O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
    
    Saída
    A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá ser apresentado com 2 casas após o ponto.

---
+++ LogicadeProgramaçãoeAlgoritmos/ExerciciosPropostos/bee1010/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Diagnostics;
+using System.Dynamic;
+
+namespace bee1010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pecas = Console.ReadLine().Split(' ');
+            int cod = int.Parse(pecas[0]);
+            int numero = int.Parse(pecas[1]);
+            double preco = double.Parse(pecas[2], CultureInfo.InvariantCulture);
+
+            pecas = Console.ReadLine().Split(' ');
+            int cod2 = int.Parse(pecas[0]);
+            int numero2 = int.Parse(pecas[1]);
+            double preco2 = double.Parse(pecas[2], CultureInfo.InvariantCulture);
+
+            double valor = (numero*preco) + (numero2*preco2);
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + valor.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
