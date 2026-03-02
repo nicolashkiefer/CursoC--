e4bb9916c6ad87da8f2c67e0f545bbdc14f64bee
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-03-02 15:55:37 -0300

    Adicionado o exercício bee1007
    Diferença
    
    Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
    
    Entrada
    O arquivo de entrada contém 4 valores inteiros.
    
    Saída
    Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.

---
+++ LogicadeProgramaçãoeAlgoritmos/ExerciciosPropostos/bee1007/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Dynamic;
+using System.Globalization;
+
+namespace bee1007
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+
+            int DIFERENCA = (A * B - C * D);
+
+            Console.WriteLine("DIFERENCA = "+ DIFERENCA);
+        }
+    }
+}
