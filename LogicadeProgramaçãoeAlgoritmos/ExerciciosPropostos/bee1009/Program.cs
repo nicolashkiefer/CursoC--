c199ec1efdded30c50090ad388bcfc2819f68272
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-03-02 16:38:57 -0300

    Adicionado o exercício bee1009
    Salário com Bônus
    
    Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês, com duas casas decimais.
    
    Entrada
    O arquivo de entrada contém um texto (primeiro nome do vendedor) e 2 valores de dupla precisão (double) com duas casas decimais, representando o salário fixo do vendedor e montante total das vendas efetuadas por este vendedor, respectivamente.
    
    Saída
    Imprima o total que o funcionário deverá receber, conforme exemplo fornecido.

---
+++ LogicadeProgramaçãoeAlgoritmos/ExerciciosPropostos/bee1009/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Dynamic;
+using System.Diagnostics;
+
+namespace bee1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Primeiro nome do vendedor: ");
+            string nome = Console.ReadLine();
+            double salarioF = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double totalV = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //15% = 0,15
+            double salario = salarioF + totalV * 0.15;
+
+            Console.WriteLine("TOTAL = R$" + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
