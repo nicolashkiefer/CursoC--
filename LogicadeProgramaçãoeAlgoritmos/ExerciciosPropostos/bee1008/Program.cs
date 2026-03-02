529943d21cf7010e8a9a9466a51f20280d56db64
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-03-02 16:06:10 -0300

    Adicionado o exercício bee1008
    Salário
    
    Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
    
    Entrada
    O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número, quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
    
    Saída
    Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.

---
+++ LogicadeProgramaçãoeAlgoritmos/ExerciciosPropostos/bee1008/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Dynamic;
+using System.Diagnostics;
+
+namespace bee1008
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            int horas = int.Parse(Console.ReadLine());
+
+            double pHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = pHora * horas;
+
+            Console.WriteLine("NUMBER = " + numero);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
