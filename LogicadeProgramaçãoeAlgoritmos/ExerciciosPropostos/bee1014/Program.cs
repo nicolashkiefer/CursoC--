60580d1fe87afb1f0a6dc435e477039f79c714a9
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-03-03 16:07:44 -0300

    Adicionado o exercício bee1014

---
+++ LogicadeProgramaçãoeAlgoritmos/ExerciciosPropostos/bee1014/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Diagnostics;
+using System.Dynamic;
+
+namespace bee1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Consumo
+            Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+
+            Entrada
+            O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km), e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+
+            Saída
+            Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem "km/l".*/
+
+            int X = int.Parse(Console.ReadLine());
+            double Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double consumo = X / Y;
+
+            Console.WriteLine(consumo.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
