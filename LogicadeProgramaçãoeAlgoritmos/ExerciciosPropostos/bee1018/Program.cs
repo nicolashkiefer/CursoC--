3e0a643d9c689218420fc3ab74985c0bcbcf195d
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-03-03 21:52:30 -0300

    Adicionado o exercício bee1018

---
+++ LogicadeProgramaçãoeAlgoritmos/ExerciciosPropostos/bee1018/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Dynamic;
+using System.Diagnostics;
+
+namespace bee1018
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            int resto = valor;
+
+            int n100 = resto / 100;
+            resto %= 100;
+
+            int n50 = resto / 50;
+            resto %= 50;
+
+            int n20 = resto / 20;
+            resto %= 20;
+
+            int n10 = resto / 10;
+            resto %= 10;
+
+            int n5 = resto / 5;
+            resto %= 5;
+
+            int n2 = resto / 2;
+            resto %= 2;
+
+            int n1 = resto;
+
+            Console.WriteLine(valor);
+            Console.WriteLine(n100 + " nota(s) de R$ 100,00");
+            Console.WriteLine(n50 + " nota(s) de R$ 50,00");
+            Console.WriteLine(n20 + " nota(s) de R$ 20,00");
+            Console.WriteLine(n10 + " nota(s) de R$ 10,00");
+            Console.WriteLine(n5 + " nota(s) de R$ 5,00");
+            Console.WriteLine(n2 + " nota(s) de R$ 2,00");
+            Console.WriteLine(n1 + " nota(s) de R$ 1,00");
+        }
+    }
+}
