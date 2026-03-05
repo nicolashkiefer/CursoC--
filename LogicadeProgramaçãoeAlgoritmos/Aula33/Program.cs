99a549721908cccd6eaf986b922b0ac6fc0ce67a
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-03-05 18:36:46 -0300

    Implementado conteúdo da aula 32 e aula 33 do curso
    Aula contêm exemplos referente aos assuntos abordados em aula sobre Expressões Lógicas e Estrutura Condicional em C#, como:
     - Operadores Lógicos
     - E / OU / NÃO
     - Tabela Verdade
     - Sintaxe da estrutura condicional
     - Encadeamento de estruturas condicionais
     - if ( <condição 1> ){
    	<comando 1>
    	<comando 2>
    } else {
    	<comando 3>
    	<comando 4>
    }

---
+++ LogicadeProgramaçãoeAlgoritmos/Aula33/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula33
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Quantas horas? ");
+            int horas = int.Parse(Console.ReadLine());
+
+            if (horas < 12){
+                Console.WriteLine("Bom dia!");
+            }
+            else {
+                if (horas < 18) {
+                    Console.WriteLine("Boa tarde!");
+                }
+                else {
+                    Console.WriteLine("Boa noite!");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
