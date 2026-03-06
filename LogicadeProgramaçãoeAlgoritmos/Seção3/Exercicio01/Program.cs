7f666cdaad6fcce3fbd55b6eb6b4f798e71e5ce6
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-03-06 15:19:17 -0300

    Implementado 5º Exercício prático do curso
    Adicionei um novo conteúdo chamado Exercicio01. Incluí os arquivos referente ao Exercicio01, que tem como objetivo realizar a impressão da nota final, mas com o novo conteúdo de IF, onde se o total for inferior a 60,  imprime "REPROVADO".

---
+++ LogicadeProgramaçãoeAlgoritmos/Seção3/Exercicio01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double notaF = nota1 + nota2;
+
+            Console.WriteLine("NOTA FINAL = " + notaF.ToString("F1", CultureInfo.InvariantCulture));
+
+            if (notaF < 60.00)
+            {
+                Console.WriteLine("REPROVADO");
+            }
+        }
+    }
+}
