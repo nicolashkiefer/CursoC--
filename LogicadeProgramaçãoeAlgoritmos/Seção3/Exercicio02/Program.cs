7cb1048735033da37b9bca49afee8ef28ca7eb1b
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-03-06 16:31:12 -0300

    Implementado 6º Exercício prático do curso
    Adiciona um novo exercício prático, onde foi realizado o cálculo de bhaskara. Resumindo, o programa lê três números de ponto flutuante de dupla precisão, calcula a fórmula quadrática (delta) e imprimem R1/R2 formatado com 5 casas decimais ou "Impossível calcular" quando a == 0 ou delta < 0.

---
+++ LogicadeProgramaçãoeAlgoritmos/Seção3/Exercicio02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valor = Console.ReadLine().Split(' ');
+            double a = double.Parse(valor[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(valor[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(valor[2], CultureInfo.InvariantCulture);
+
+            double delta = Math.Pow(b, 2.0) - 4*a*c;
+
+            if (a == 0 || delta < 0.0)
+            {
+                Console.WriteLine("Impossível calcular");
+            }
+            else
+            {
+                double r1 = (- b + Math.Sqrt(delta)) / (2.0*a); 
+                double r2 = (- b - Math.Sqrt(delta)) / (2.0*a);
+
+                Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
