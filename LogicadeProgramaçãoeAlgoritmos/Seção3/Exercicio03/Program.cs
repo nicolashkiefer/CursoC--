c7843104cc6e82b7fd960815402574d7cf16d4d9
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2026-03-06 18:53:44 -0300

    Implementado parte do 6º Exercício prático do curso

---
+++ LogicadeProgramaçãoeAlgoritmos/Seção3/Exercicio03/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string[] numero = Console.ReadLine().Split(' ');
+            int x = int.Parse(numero[0]);
+            int y = int.Parse(numero[1]);
+            int z = int.Parse(numero[2]);
+
+            int menor;
+
+            if (x == y && y == z)
+            {
+                menor == x;
+            }
+            else if (x < y && x < z)
+            {
+                menor == x;
+            }
+            else if (y < x && y < z)
+            {
+                menor == y;
+            }
+            else
+            {
+                menor == z;
+            }
+
+
+
+        }
+    }
+}
